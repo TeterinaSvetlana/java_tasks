a13a79b828dde9e263428014164a11113940562c
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2021-02-01 19:47:16 +0500

    task 2.5

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,81 @@
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        // 1.
-        String [] words = {"one", "two", "three", "four", "five",
-                            "one", "two", "six", "four", "five",
-                            "one", "two", "seven", "four", "five",};
-        HashSet<String> hashSetWords = new HashSet<String>(Arrays.asList(words));
-        System.out.println(hashSetWords);
+    oneWholeArray();
+    arraySplitToThreads();
 
-        for (String el: hashSetWords) {
-            int k = 0;
-            for (int j = 0; j < words.length; j++) {
-                if (el == words[j]) k++;
-            }
-            System.out.println(el + ": " + k);
+    }
+
+    public static void oneWholeArray () {
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
         }
 
-        // 2.
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Ivanov", "89081234312");
-        phoneBook.add("Ketov", "89121231745");
-        phoneBook.add("Ivanov", "89511270310");
+        long a = System.currentTimeMillis();
 
-        phoneBook.get("Ivanov");
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[5000000]);
+
+    }
+
+    public static void arraySplitToThreads () {
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < h; i++) {
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < h; i++) {
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + (i+h) / 5) * Math.cos(0.2f + (i+h) / 5) * Math.cos(0.4f + (i+h) / 2));
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - a);
+        System.out.println(arr[5000000]);
+
     }
 }
